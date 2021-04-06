e90af5c7a7694eee00ca2cee90892c24d5e63dda
trungbe <trungnd0601@gmail.com>
2021-04-06 09:07:35 +0700

    component font size

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FontSizeEditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
